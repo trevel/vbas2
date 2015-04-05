0eba8263935f833065fd6217f58a42064dd6dc1b
llshields <laurie_shields@hotmail.com>
2015-04-05 12:31:03 -0400

    Added more crud interfaces to DBAccessHelper

--- DBAccessLib/DBAccessHelper.vb
+++ DBAccessLib/DBAccessHelper.vb
@@ -1,4 +1,9 @@
-﻿Imports System.Data.SqlClient
+﻿' '*****************************************************************************************
+' Student Names: Laurie Shields (034448142)
+'                Mark Lindan (063336143)
+' CVB815 - DBAccessHelper
+'*******************************************************************************************
+Imports System.Data.SqlClient
 Imports Database
 Imports System.Configuration
 Public Class DBAccessHelper
@@ -19,8 +24,9 @@ Public Class DBAccessHelper
         End Try
     End Sub
 
-    Private Shared Sub DBSQLExecute(conn As SqlClient.SqlConnection, cmd As SqlCommand)
-
+    ' Helper method to manage the transaction and handle commit or rollback
+    Private Shared Function DBSQLExecute(conn As SqlClient.SqlConnection, cmd As SqlCommand) As Boolean
+        Dim bRetVal As Boolean = False
         '# Using SqlTransaction: BeginTransaction, Commit, Rollback
         Dim transaction As SqlTransaction       '# setup for local transaction
         transaction = Nothing
@@ -31,14 +37,17 @@ Public Class DBAccessHelper
             cmd.Transaction = transaction           '# setup command transaction
             cmd.ExecuteScalar()                     '# call SQL Stored procedure
             transaction.Commit()                    '# commit transaction
+            bRetVal = True
         Catch sqlExcept As SqlException          '# transaction failure
             If transaction IsNot Nothing Then
                 transaction.Rollback()          '# rollback transaction
             End If
         End Try
-    End Sub
+        Return bRetVal
+    End Function
 
     ' LAURIE - TODO:: This function is only for unit test purposes
+    ' return Nothing if product not found, otherwise returns a the Product
     Public Shared Function DBReadProductByID(id As Integer) As Product
         ' Get the connection
         Dim conn As SqlClient.SqlConnection = DBAccessHelper.DBGetConnection()
@@ -50,7 +59,7 @@ Public Class DBAccessHelper
         cmd.CommandType = CommandType.StoredProcedure
         cmd.CommandText = "dbo.sp_read_product"
 
-        ' Set up the input parameters and values
+        ' Set up the input and output parameters
         cmd.Parameters.Add("@id", SqlDbType.Int)
         cmd.Parameters("@id").Direction = ParameterDirection.InputOutput
         cmd.Parameters("@id").Value = id
@@ -66,20 +75,17 @@ Public Class DBAccessHelper
         Try
             conn.Open()
             cmd.ExecuteScalar()                     '# call SQL Stored procedure
-            id = cmd.Parameters("@id").Value
-            If id = -1 Then
-                ' couldn't find record
-            Else
-                p = New Product()
-                p.ID = id
-                p.Description = cmd.Parameters("@desc").Value
-                p.Price = cmd.Parameters("@price").Value
-                p.Inventory = cmd.Parameters("@inv").Value
-                p.active = cmd.Parameters("@active").Value
+            id = cmd.Parameters("@id").Value        ' retrieve the in/out param value
+            If id <> -1 Then
+                ' found the record, create and populate the new product object
+                p = New Product(id, cmd.Parameters("@desc").Value,
+                                cmd.Parameters("@price").Value,
+                                cmd.Parameters("@inv").Value,
+                                cmd.Parameters("@active").Value)
             End If
 
-        Catch sqlExcept As SqlException          '# transaction failure
-            ' LAURIE - TODO add something here
+        Catch sqlExcept As SqlException
+            ' nothing to do here...since the read is just a query
         Finally
             DBAccessHelper.DBConnectionClose(conn)
         End Try
@@ -114,18 +120,22 @@ Public Class DBAccessHelper
         Else
             cmd.Parameters("@active").Value = 0
         End If
+        ' set up the output parameter that we use to extract the id
         cmd.Parameters.Add("@id", SqlDbType.Int)
         cmd.Parameters("@id").Direction = ParameterDirection.Output
-        DBAccessHelper.DBSQLExecute(conn, cmd)
-        retVal = cmd.Parameters("@id").Value
+        If DBAccessHelper.DBSQLExecute(conn, cmd) = True Then
+            retVal = cmd.Parameters("@id").Value
+        End If
         DBAccessHelper.DBConnectionClose(conn)
         conn = Nothing
         Return retVal
     End Function
 
-    Public Shared Sub DBDeleteProduct(p As Product)
+    ' true on success, false on fail
+    Public Shared Function DBDeleteProduct(p As Product) As Boolean
+        Dim bRetVal As Boolean = False
         If p Is Nothing Then
-            Return
+            Return bRetVal
         End If
         ' Get the connection
         Dim conn As SqlClient.SqlConnection = DBAccessHelper.DBGetConnection()
@@ -140,14 +150,19 @@ Public Class DBAccessHelper
         cmd.Parameters.Add("@id", SqlDbType.Int)
         cmd.Parameters("@id").Value = p.ID
 
-        DBAccessHelper.DBSQLExecute(conn, cmd)
+        If DBAccessHelper.DBSQLExecute(conn, cmd) = True Then
+            bRetVal = True
+        End If
         DBAccessHelper.DBConnectionClose(conn)
         conn = Nothing
-    End Sub
+        Return bRetVal
+    End Function
 
-    Public Shared Sub DBUpdateProduct(p As Product)
+    ' true on success, false on fail
+    Public Shared Function DBUpdateProduct(p As Product) As Boolean
+        Dim bRetVal As Boolean = False
         If p Is Nothing Then
-            Return
+            Return bRetVal
         End If
         ' Get the connection
         Dim conn As SqlClient.SqlConnection = DBAccessHelper.DBGetConnection()
@@ -169,9 +184,92 @@ Public Class DBAccessHelper
         cmd.Parameters("@inv").Value = p.Inventory
         cmd.Parameters.Add("@active", SqlDbType.Bit)
         cmd.Parameters("@active").Value = p.active
-        DBAccessHelper.DBSQLExecute(conn, cmd)
+        If DBAccessHelper.DBSQLExecute(conn, cmd) = True Then
+            bRetVal = True
+        End If
         DBAccessHelper.DBConnectionClose(conn)
         conn = Nothing
-    End Sub
+        Return bRetVal
+    End Function
+
+    ' return Nothing if address not found, otherwise returns an Address
+    Public Shared Function DBReadAddressByID(id As Integer) As Address
+        Return Nothing
+    End Function
+
+    ' return the product id of the new item, -1 on fail
+    Public Shared Function DBInsertAddress(a As Address) As Integer
+        Return -1
+    End Function
+
+    ' true on success, false on fail
+    Public Shared Function DBDeleteAddress(a As Address) As Boolean
+        Return False
+    End Function
+
+    ' true on success, false on fail
+    Public Shared Function DBUpdateAdddress(a As Address) As Boolean
+        Return False
+    End Function
+
+    ' return Nothing if customer not found, otherwise returns a Customer
+    Public Shared Function DBReadCustomerByID(id As Integer) As Customer
+        Return Nothing
+    End Function
+
+    ' return the product id of the new item, -1 on fail
+    Public Shared Function DBInsertCustomer(c As Customer) As Integer
+        Return -1
+    End Function
+
+    ' true on success, false on fail
+    Public Shared Function DBDeleteCustomer(c As Customer) As Boolean
+        Return False
+    End Function
+
+    ' true on success, false on fail
+    Public Shared Function DBUpdateCustomer(c As Customer) As Boolean
+        Return False
+    End Function
+
+    ' return Nothing if order not found, otherwise returns an Order
+    Public Shared Function DBReadOrderByID(id As Integer) As Order
+        Return Nothing
+    End Function
+
+    ' return the product id of the new item, -1 on fail
+    Public Shared Function DBInsertOrder(o As Order) As Integer
+        Return -1
+    End Function
+
+    ' true on success, false on fail
+    Public Shared Function DBDeleteOrder(o As Order) As Boolean
+        Return False
+    End Function
+
+    ' true on success, false on fail
+    Public Shared Function DBUpdateOrder(o As Order) As Boolean
+        Return False
+    End Function
+
+    ' return Nothing if orderitem not found, otherwise returns an OrderItem
+    Public Shared Function DBReadOrderItemByID(id As Integer) As OrderItem
+        Return Nothing
+    End Function
+
+    ' return the product id of the new item, -1 on fail
+    Public Shared Function DBInsertOrderItem(i As OrderItem) As Integer
+        Return -1
+    End Function
+
+    ' true on success, false on fail
+    Public Shared Function DBDeleteOrderItem(i As OrderItem) As Boolean
+        Return False
+    End Function
+
+    ' true on success, false on fail
+    Public Shared Function DBUpdateOrderItem(i As OrderItem) As Boolean
+        Return False
+    End Function
 End Class
 
